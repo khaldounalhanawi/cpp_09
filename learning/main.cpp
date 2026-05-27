991909178eea4b5552a3868d1b06e15d1e8d685c
Khaldoun Alhanawi <khaldounalhanawi@users.noreply.github.com>
2026-05-27 13:07:13 +0200

    clean

--- learning/main.cpp
+++ learning/main.cpp
@@ -11,7 +11,7 @@
 #include <ctime>
 #include <exception>
 
-void	tester(t_vector_ints a, t_vector_ints b)
+void	tester(const t_vector_ints &a, const t_vector_ints &b)
 {
 	if (a == b)
 		std::cout << "MATCH!" << std::endl;
@@ -19,7 +19,7 @@ void	tester(t_vector_ints a, t_vector_ints b)
 		std::cout << "xxxx FAIL xxxx" << std::endl;
 }
 
-long	getTimePercise()
+long	getTimePrecise()
 {
 	timeval	tp;
 
@@ -32,13 +32,13 @@ long	getTimePercise()
 	return (realReadUs);
 }
 
-void	returnErrorMsg(std::string message, int errorNumber)
+void	returnErrorMsg(const std::string &message, int errorNumber)
 {
 	std::cerr	<<	message	<< std::endl;
 	std::exit (errorNumber);
 }
 
-int	safeAtol(char *str)
+int	safeAtol(const char *str)
 {
 	char	*end = NULL;
 	errno = 0;
@@ -70,45 +70,47 @@ t_vector_ints	parseAsVector(int argc, char **argv)
 	return (values);
 }
 
-
- #include <unistd.h>
 int	main(int argc, char **argv)
 {
-	(void)argv;
-
 	// checks
 	if (argc < 2)
 		returnErrorMsg ("You must provide atleast 1 argument to program", 1);
 
 	// record time
-	long firstRead = getTimePercise();
+	long firstRead = getTimePrecise();
 
-	// parse input
+	// Vector operations
 	t_vector_ints input = parseAsVector (argc, argv);
 	t_vector_ints ordered = FordJohnsonSort (input);
 
-	// get time
-	long secondRead = getTimePercise();
-	
+	// record time
+	long secondRead = getTimePrecise();
 
-	// calculate time
+	// calculate duration for Vector operations
 	long duration = secondRead - firstRead;
 
 	// deque thingy
 
-	// print outs
+	// test correctness
 	t_vector_ints computerSorted (input);
 	std::sort (computerSorted.begin(), computerSorted.end());
 	tester (ordered, computerSorted);
 
+	// print Before/After
 	std::cout	<< "Before:";
 	printVectorInts (input);
 	std::cout	<< "After:";
 	printVectorInts (ordered);
-	std::cout	<< "Time to process a range of "	<< input.size()	<< " elements with std::vector : " << duration << " us\n";
-	std::cout	<< "Time to process a range of "	<< input.size()	<< " elements with std::deque :" << std::endl;
-}
 
+	// print Benchmarks
+	std::cout	<< "Time to process a range of "
+				<< input.size()
+				<< " elements with std::vector : "
+				<< duration
+				<< " us\n";
+
+	return (0);
+}
 
 // ./PmergeMe `shuf -i 1-100000 -n 3000 | tr "\n" " "`
 // make: warning:  Clock skew detected.  Your build may be incomplete.
@@ -119,37 +121,3 @@ After: 3 4 5 7 9
 Time to process a range of 5 elements with std::[..] : 0.00031 us
 Time to process a range of 5 elements with std::[..] : 0.00014 us
 */
-
-// int	main()
-// {
-// 	t_vector_ints	input;
-// 	t_vector_ints	ordered;
-
-// 	input.push_back (4);
-// 	input.push_back (1);
-// 	input.push_back (4);
-// 	input.push_back (1);
-// 	input.push_back (7);
-// 	input.push_back (5);
-// 	input.push_back (1);
-// 	input.push_back (3);
-// 	input.push_back (2);
-// 	input.push_back (0);
-// 	input.push_back (8);
-// 	input.push_back (6);
-// 	input.push_back (9);
-// 	input.push_back (0);
-// 	input.push_back (22);
-// 	input.push_back (-45);
-
-// 	t_vector_ints	comparasion(input);
-// 	std::sort (comparasion.begin(), comparasion.end());
-
-// 	ordered = FordJohnsonSort (input);
-
-// 	tester (ordered, comparasion);
-// 	printVectorInts (ordered);
-// 	printVectorInts (comparasion);
-
-// 	return (0);
-// }
